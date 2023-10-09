31f4c9d0c7fcd501dadee0163c3de766c3b9e849
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-10-09 13:21:02 -0300

    :sparkles: feat: Animations on pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,46 @@
 const loginBox = document.querySelector('#login');
 const signupBox = document.querySelector('#signup');
-const newAccountButton = document.querySelector('[data-create-account]');
-const loginButton = document.querySelector('[data-login-account]');
+const welcomeBox = document.querySelector('#welcome');
 
-newAccountButton.addEventListener('click', () => {
+const newAccountButton = document.querySelector('[data-create-account]');
+const loginAccountButton = document.querySelector('[data-login-account]');
+const logoutButton = document.querySelector('[data-logout-account]');
+
+const signupButton = document.querySelector('[data-form-signup]');
+const loginButton = document.querySelector('[data-form-login]');
+
+function loginSuccess () {
+    loginBox.style.display = 'none';
+    signupBox.style.display = 'none';
+    welcomeBox.style.display = 'flex';
+}
+
+function createAccount () {
     loginBox.style.display = 'none';
     signupBox.style.display = 'flex';
-})
+    welcomeBox.style.display = 'none';
+}
 
-loginButton.addEventListener('click', () => {
+function loginAccount () {
     loginBox.style.display = 'flex';
     signupBox.style.display = 'none';
+    welcomeBox.style.display = 'none';
+}
+
+newAccountButton.addEventListener('click', createAccount);
+loginAccountButton.addEventListener('click', loginAccount);
+logoutButton.addEventListener('click', loginAccount);
+
+signupButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    signupBox.style.display = 'none';
+    welcomeBox.style.display = 'flex';
+})
+
+loginButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    loginBox.style.display = 'none';
+    welcomeBox.style.display = 'flex';
 })
